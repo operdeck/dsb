7bafa8fe3f4142a37012cf97216dfc3ef930be86
Otto Perdeck <perdo@pega.com>
2016-01-16 20:58:48 +0100

    Alternative way getting image list

--- playlist.R
+++ playlist.R
@@ -10,94 +10,60 @@ source("Util.R")
 # removed slicelist.csv from git, add imagelist.csv
 # FileName added
 # for all image types
+# Slice itself not so relevant?
 
-listSliceImages <- function(sliceInfo) {
-  imgFolder <- getImageFolder(sliceInfo)
-  imgFiles <- list.files(imgFolder, pattern=".*\\.dcm$")
-  imageMetaData <- data.table(sliceInfo,
+print("Listing image files")
+filez <- list.files("data", pattern=".*\\.dcm", recursive=T)
+filePattern <- "^(.*)/([[:digit:]]+)/study/(.+)_([[:digit:]]+)/IM-(\\d{1,5})-(\\d{1,5})-{0,1}(\\d{0,5})\\.dcm$"
+playlist <- data.table(FileName = filez,
+                       Dataset = gsub(filePattern, "\\1", filez),
+                       Id = as.numeric(gsub(filePattern, "\\2", filez)),
+                       ImgType = gsub(filePattern, "\\3", filez),
+                       Slice = as.numeric(gsub(filePattern, "\\4", filez)),
+                       Offset = as.numeric(gsub(filePattern, "\\5", filez)),
+                       Time = as.numeric(gsub(filePattern, "\\6", filez)),
+                       SubSlice = as.numeric(gsub(filePattern, "\\7", filez)))
+playlist$Slice <- ifelse(is.na(playlist$SubSlice), playlist$Slice, playlist$Slice*1000 + playlist$SubSlice)
+playlist <- select(playlist, -SubSlice) %>% arrange(Dataset, Id, ImgType, Slice)
+setkey(playlist, Dataset, Id, ImgType, Slice)
+
+print("Creating slice groups")
+sliceMetaData <- select(playlist, Dataset, Id, ImgType, Slice) %>% unique() %>% arrange(Dataset, Id, ImgType, Slice)
+sliceMetaData$One <- 1 # trick to do a count within the := operator of data.table
+sliceMetaData[, 
+            c("SliceCount", "SliceIndex", "SliceOrder") := list(sum(One), seq(sum(One)), midOrderSeqKeepSibblingsTogether(sum(One))),
+            by=c("Dataset", "Id", "ImgType")]
+playlist <- left_join(playlist, sliceMetaData, by=c("Dataset", "Id", "ImgType", "Slice"))
+
+print("Reading image data")
+for (n in seq(nrow(playlist))) {
+  img <- playlist[n,]
+  print(img)
+  dicomHeader <- readDICOMFile(paste("data", img$FileName, sep="/"), pixelData = FALSE)
+  pixelSpacing <- extractHeader(dicomHeader$hdr, "PixelSpacing", numeric=FALSE)
   
-                              FileName = imgFiles,
-                              
-                              # this will not work for some images that have
-                              # a different format
-                              Offset = as.integer(gsub("^IM-(\\d{4,})-(\\d{4})\\.dcm$", "\\1", imgFiles)),
-                              Time = as.integer(gsub("^IM-(\\d{4,})-(\\d{4})\\.dcm$", "\\2", imgFiles)),
-                              
-                              # meta info
-                              PixelSpacing.x = rep(NA, length(imgFiles)),
-                              PixelSpacing.y = rep(NA, length(imgFiles)),
-                              SliceLocation = rep(NA, length(imgFiles)),
-                              SliceThickness = rep(NA, length(imgFiles)))
-
-  for (i in seq(nrow(imageMetaData))) {
-    dicomHeader <- readDICOMFile(paste(getImageFolder(imageMetaData[i,]),imageMetaData$FileName[i],sep="/"), 
-                                 pixelData = TRUE)
-    pixelSpacing <- extractHeader(dicomHeader$hdr, "PixelSpacing", numeric=FALSE)
-    
-    imageMetaData$PixelSpacing.x[i] <- as.numeric(gsub("^(.*) (.*)$", "\\1", pixelSpacing))
-    imageMetaData$PixelSpacing.y[i] <- as.numeric(gsub("^(.*) (.*)$", "\\2", pixelSpacing))
-    imageMetaData$SliceLocation[i]  <- extractHeader(dicomHeader$hdr, "SliceLocation", numeric=TRUE)
-    imageMetaData$SliceThickness[i] <- extractHeader(dicomHeader$hdr, "SliceThickness", numeric=TRUE)
-    img <- Image(dicomHeader$img)
-    if (dim(img)[1] > dim(img)[2]) {
-      imageMetaData$Dim.x <- dim(img)[1]
-      imageMetaData$Dim.y <- dim(img)[2]
-    } else {
-      imageMetaData$Dim.x <- dim(img)[2]
-      imageMetaData$Dim.y <- dim(img)[1]
-    }
-  }
-  
-  return(imageMetaData)
+  playlist$PixelSpacing.x[n] <- as.numeric(gsub("^(.*) (.*)$", "\\1", pixelSpacing))
+  playlist$PixelSpacing.y[n] <- as.numeric(gsub("^(.*) (.*)$", "\\2", pixelSpacing))
+  playlist$SliceLocation[n]  <- extractHeader(dicomHeader$hdr, "SliceLocation", numeric=TRUE)
+  playlist$SliceThickness[n] <- extractHeader(dicomHeader$hdr, "SliceThickness", numeric=TRUE)
 }
-
-playlist <- NULL
-for (dataset in getAllDatasetFolders()) {
-  datasetDir <- paste("data",dataset,sep="/")
-  Ids <- sort(as.integer(list.dirs(datasetDir, recursive=F, full.names=F)))
-  for (Id in Ids) {
-    IdFolder <- paste(datasetDir, Id, "study", sep="/")
-    subFolders <- list.dirs(IdFolder, recursive=F, full.names=F)
-    sliceMetaData <- data.table(Id = Id, 
-                                Dataset = dataset,
-                                ImgType = gsub("(.+)_([[:digit:]]+)$", "\\1", subFolders),
-                                Slice = as.integer(gsub("(.+)_([[:digit:]]+)$", "\\2", subFolders))) %>%
-      arrange(Dataset, ImgType, Slice)
-    for (t in unique(sliceMetaData$ImgType)) {
-      sliceMetaDataByImgType <- filter(sliceMetaData, ImgType==t)
-      
-      sliceMetaDataByImgType$SliceCount = nrow(sliceMetaDataByImgType)
-      sliceMetaDataByImgType$SliceIndex = seq(nrow(sliceMetaDataByImgType))
-      sliceMetaDataByImgType$SliceOrder = midOrderSeqKeepSibblingsTogether(nrow(sliceMetaDataByImgType))
-      
-      print(sliceMetaDataByImgType)
-      for (s in seq(nrow(sliceMetaDataByImgType))) {
-        imageMetaData <- listSliceImages(sliceMetaDataByImgType[s,])
-        
-        if (is.null(playlist)) {
-          playlist <- imageMetaData
-        } else {
-          playlist <- rbind(playlist, imageMetaData)
-        }
-      }
-    }
-    write.csv(playlist, "imagelist.csv", row.names=F)
-  }
-}
+# TODO - fill in missings
+write.csv(playlist, "imagelist.csv", row.names=F)
 
 getImageList <- function(type = "sax")
 {
   playlist <- fread("imagelist.csv")
   r <- filter(playlist, ImgType==type)
-  setkey(r, Id, Dataset, ImgType, Slice)
+  setkey(r, Id, Dataset, ImgType, Slice, FileName)
   return(r)
 }
 
 getSliceList <- function(type = "sax")
 {
-  slices <- select(getImageList(type), Id, Dataset, ImgType, Slice, Offset, starts_with("slice")) %>% unique()
-  setkey(slices, Id, Dataset, ImgType, Slice)
-  return(slices)
+  oneSliceGroup <- select(getImageList(type), Id, Dataset, ImgType, Offset, starts_with("Slice")) %>% 
+    arrange(Dataset, Id, ImgType, Slice, SliceLocation) %>% unique()
+  setkey(oneSliceGroup, Dataset, Id, ImgType, Slice, SliceLocation)
+  return(oneSliceGroup)
 }
 
 getIdList <- function(type = "sax")
