12e0dd985a1638852e7c4c97431452e4c1d96804
Otto from Mac <operdeck@gmail.com>
2016-01-19 09:27:42 +0100

    Minor

--- extract.R
+++ extract.R
@@ -208,7 +208,7 @@ for (nextSlice in seq(nrow(sliceList))) {
     }
     
     # Add segmentation to results and write file (once in a while)
-    if ((nextSlice %% 10 == 0) | T | 
+    if ((nextSlice %% 10 == 0) | 
           (nextSlice == nrow(sliceList)) | 
           ((nextSlice < nrow(sliceList)) & (sliceList$Dataset[nextSlice+1] != sliceList$Dataset[nextSlice]))) {
       ds <- sliceList$Dataset[nextSlice]
