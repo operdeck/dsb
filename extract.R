86d779395e9c04a57e9cac4dcb1ef94b9f8962c9
Otto from Mac <operdeck@gmail.com>
2016-01-29 08:22:47 +0100

    Working through the segment field changes

--- extract.R
+++ extract.R
@@ -27,7 +27,7 @@ source("util.R")
 # see bio image detection stuff
 # http://bioconductor.wustl.edu/bioc/vignettes/EBImage/inst/doc/AnalysisWithEBImage.pdf
 
-segmentImagesForOneSlice <- function(imgMetaData) {
+segmentImagesForOneSlice <- function(imgMetaData, prevROI=NULL) {
   allImages <- vector(mode = "list", length = nrow(imgMetaData))
   for (i in seq(nrow(imgMetaData))) {
     f <- paste("data",imgMetaData$FileName[i],sep="/")
@@ -43,40 +43,55 @@ segmentImagesForOneSlice <- function(imgMetaData) {
     }
     allImages[[i]] <- img
   }
-  # Calculate 'average' image
-  isFirst <- T
-  for (i in allImages) {
-    if (isFirst) {
-      avgImg <- i
-      isFirst <- F
-    } else {
-      avgImg <- avgImg + i
-    }
-  }
-  avgImg <- avgImg / length(allImages)
+  
 
+  if (is.null(prevROI)) {
+    # Calculate 'average' image
+    isFirst <- T
+    for (i in allImages) {
+      if (isFirst) {
+        avgImg <- i
+        isFirst <- F
+      } else {
+        avgImg <- avgImg + i
+      }
+    }
+    avgImg <- avgImg / length(allImages)
+  }
+  
   sliceSegmentation <- NULL
   for (i in 1:nrow(imgMetaData)) {
     pixelAreaScale <- imgMetaData$PixelSpacing.x[i] * imgMetaData$PixelSpacing.y[i]
     pixelLengthScale <- sqrt(pixelAreaScale)
     
     img_original <- allImages[[i]] 
-    img_subtracted <- normalize(abs(img_original - avgImg))
-    img_roi_thresholded <- (img_subtracted > otsu(img_subtracted))
-    # 
-    img_roi <- medianFilter(img_roi_thresholded, min(1,round(1.5/pixelLengthScale))) # 2 for "normal images" scale 0.75
     
-    # Find the ROI
-    roi = as.data.frame(computeFeatures.moment(img_roi))
-    coords <- as.data.frame(pos2coord(pos=which(img_roi>0),dim.mat=dim(img_roi)))
-    names(coords) <- c('x','y')
-    coords$distToROI <- floor(sqrt((coords$x - roi$m.cx)^2+(coords$y - roi$m.cy)^2))
-    roi$radius <- quantile(coords$distToROI, probs=c(0.90)) # important constant: what to consider not moving
+    if (is.null(prevROI)) {    
+      img_roi_subtracted <- normalize(abs(img_original - avgImg))
+      img_roi_thresholded <- (img_roi_subtracted > otsu(img_roi_subtracted))
+      img_roi <- medianFilter(img_roi_thresholded, min(1,round(1.5/pixelLengthScale))) # 2 for "normal images" scale 0.75
+      
+      # Find the ROI
+      roi <- as.data.frame(computeFeatures.moment(img_roi))
+      coords <- as.data.frame(pos2coord(pos=which(img_roi>0),dim.mat=dim(img_roi)))
+      names(coords) <- c('x','y')
+      coords$distToROI <- floor(sqrt((coords$x - roi$m.cx)^2+(coords$y - roi$m.cy)^2))
+      roi$radius <- quantile(coords$distToROI, probs=c(0.80)) # important constant: what to consider not moving
+    } else {
+      roi <- data.frame(m.cx = prevROI$x, m.cy = prevROI$y, radius = prevROI$r)
+    }    
+    
     if (roi$radius > 1) {
-      roi_mask <- matrix(0, nrow=dim(img_roi)[1], ncol=dim(img_roi)[2])
+      roi_mask <- matrix(0, nrow=dim(img_original)[1], ncol=dim(img_original)[2])
       roi_mask <- drawCircle(roi_mask, x=roi$m.cx, y=roi$m.cy, roi$radius, col=1, fill=T)
       img_masked <- img_original*roi_mask
-      scale <- 1/max(img_masked)
+      
+      #print(summary(img_original))
+      #print(summary(roi_mask))
+      cat("+++ scale max",max(img_masked),"80%",quantile(img_masked,0.80),fill=T)
+      #scale <- 1/max(img_masked)
+      scale <- min(c(1/quantile(img_masked,0.80),1/max(img_masked)))
+      
       img_original <- img_original*scale # scale original image wrt ROI
       
       kern <- makeBrush(min(1,round(3.75/pixelLengthScale)), shape= 'disc') # 5 for "normal images" scale 0.75
@@ -103,13 +118,20 @@ segmentImagesForOneSlice <- function(imgMetaData) {
       img_colourSegs <- colorLabels(rmObjects(img_segmented, 
                                               setdiff(seq(max(img_segmented)),segmentInfo$segIndex)))
       
-      img_comb <- EBImage::combine(drawCircle(img_colourSegs, x=roi$m.cx, y=roi$m.cy, roi$radius, "yellow", fill=FALSE, z=1),
-                                   drawCircle(toRGB(img_original), x=roi$m.cx, y=roi$m.cy, roi$radius, "yellow", fill=FALSE, z=1),
-                                   toRGB(img_subtracted),
-                                   #toRGB(img_masked),
-                                   toRGB(img_blurred),
-                                   toRGB(img_thresholded),
-                                   toRGB(img_roi))
+      if (is.null(prevROI)) {
+        img_comb <- EBImage::combine(drawCircle(img_colourSegs, x=roi$m.cx, y=roi$m.cy, roi$radius, "yellow", fill=FALSE, z=1),
+                                     drawCircle(toRGB(img_original), x=roi$m.cx, y=roi$m.cy, roi$radius, "yellow", fill=FALSE, z=1),
+                                     toRGB(img_roi_subtracted),
+                                     #toRGB(normalize(img_masked)),
+                                     toRGB(img_blurred),
+                                     toRGB(img_thresholded),
+                                     toRGB(img_roi))
+      } else {
+        img_comb <- EBImage::combine(drawCircle(img_colourSegs, x=roi$m.cx, y=roi$m.cy, roi$radius, "yellow", fill=FALSE, z=1),
+                                     drawCircle(toRGB(img_original), x=roi$m.cx, y=roi$m.cy, roi$radius, "yellow", fill=FALSE, z=1),
+                                     toRGB(img_blurred),
+                                     toRGB(img_thresholded))
+      }
       display(img_comb,all=T,method="raster")
       text(10,20,imgMetaData$FileName[i],col="yellow",pos=4)
       
@@ -151,88 +173,117 @@ for (dataset in unique(imageList$Dataset)) {
   }
 }
 
-
 print("Starting to process images")
 # Here we order the imageList:
 # - first incomplete ID's, then unprocessed slices by their special 'mid order'
 if (!is.null(allSegments)) {
   imageList <- left_join(imageList, 
-                         mutate(unique(select(allSegments, Id, Dataset, ImgType, Slice, Time)), isProcessed=T),
-                         by=c("Dataset", "Id", "ImgType", "Slice", "Time"))
+                         mutate(unique(select(allSegments, Id, Slice, Time)), isProcessed=T),
+                         by=c("Id", "Slice", "Time"))
   imageList$isProcessed <- ifelse(is.na(imageList$isProcessed), F, T)
 } else {
   imageList$isProcessed <- F
 }
 imageList$Random <- runif(max(imageList$Id))[imageList$Id] # keep ID's together
+
+# imageList$SpecialOrder <- ifelse(imageList$SliceOrder > 1, imageList$SliceOrder, 10) # first last - temporary
+
 imageList <- arrange(imageList, isProcessed, SliceOrder, Dataset, Random) %>% select(-Random)
 
 # Process images per slice. Image of the same slice (usually) have same dimensions, location etc
+
 sliceList <- unique(select(imageList, Dataset, Id, ImgType, starts_with("Slice")))
+
 for (nextSlice in seq(nrow(sliceList))) {
   slice <- sliceList[nextSlice,]
   sliceImgMetaData <- left_join(select(slice, Dataset, Id, ImgType, Slice), 
                                 imageList,
                                 by=c("Dataset", "Id", "ImgType", "Slice")) %>% arrange(Time)
   
-  if (all(file.exists(paste("data",sliceImgMetaData$FileName,sep="/")))) { # just in case not all files are on this filesystem
-    cat("Processing",slice$Dataset,slice$Id,"slice",slice$Slice,
-        paste("(",slice$SliceIndex, "/", slice$SliceCount, " order:", slice$SliceOrder, ")", sep=""),
-        fill=T)
-    
-    sliceSegmentationInfo <- segmentImagesForOneSlice(sliceImgMetaData)
-    cat("...",nrow(sliceSegmentationInfo),"segments in",nrow(sliceImgMetaData),"images",fill=T)
-    
-    if (is.null(allSegments)) {
-      allSegments <- sliceSegmentationInfo
-    } else {
-      removedSet <- setdiff(names(allSegments), names(sliceSegmentationInfo))
-      addedSet <- setdiff(names(sliceSegmentationInfo), names(allSegments))
-      diffSet <- paste(c(paste("-",removedSet), paste("+",addedSet)),collapse=", ")
-      if (length(removedSet) + length(addedSet) > 0) {
-        print("Existing:")
-        print(names(allSegments))
-        print("New:")
-        print(names(sliceSegmentationInfo))
-        print(paste(diffSet, collapse=","))
-        stop("Datasets do not match up. Please consider removing segment files.")
-      }
-      
-      nDrop <- nrow(filter(allSegments, 
-                           Slice %in% sliceSegmentationInfo$Slice,
-                           Id %in% sliceSegmentationInfo$Id))
-      if (nDrop > 0) {
-        cat("...dropping",nDrop,"rows from previous results",fill=T)
-      }
-      
-      allSegments <- rbind(filter(allSegments, 
-                                          !(Slice %in% sliceSegmentationInfo$Slice & 
-                                              Id %in% sliceSegmentationInfo$Id)), 
-                                   sliceSegmentationInfo)
+  if (!all(file.exists(paste("data",sliceImgMetaData$FileName,sep="/")))) { 
+    # just in case not all files are on this filesystem
+    next
+  }
+  cat("Processing",slice$Dataset,slice$Id,"slice",slice$Slice,
+      paste("(",slice$SliceIndex, "/", slice$SliceCount, " order:", slice$SliceOrder, ")", sep=""),
+      fill=T)
+
+  # ROI of all slices of the same Id with lower order (closer to the middle)
+  ROIs <- filter(left_join(select(filter(allSegments, Id==slice$Id, Slice!=slice$Slice), 
+                                  Id, Slice, starts_with("ROI.")),
+                           imageList, by=c("Id", "Slice")), 
+                 SliceOrder < slice$SliceOrder)
+  if (nrow(ROIs)>0) {
+    roi <- list(x = mean(ROIs$ROI.x), y = mean(ROIs$ROI.y), r = mean(ROIs$ROI.r))
+  } else {
+    roi <- NULL
+  }
+  
+  sliceSegmentationInfo <- segmentImagesForOneSlice(sliceImgMetaData, roi)
+  # drop meta-data as this will be joined in via image list upon read
+  sliceSegmentationInfo <- select(sliceSegmentationInfo, 
+                                  Id, Slice, Time, segIndex, UUID,
+                                  starts_with("m."), starts_with("s."),
+                                  starts_with("ROI."))
+  cat("...",nrow(sliceSegmentationInfo),"segments in",nrow(sliceImgMetaData),"images",fill=T)
+  
+  if (is.null(allSegments)) {
+    allSegments <- sliceSegmentationInfo
+  } else {
+    removedSet <- setdiff(names(allSegments), names(sliceSegmentationInfo))
+    addedSet <- setdiff(names(sliceSegmentationInfo), names(allSegments))
+    diffSet <- paste(c(paste("-",removedSet), paste("+",addedSet)),collapse=", ")
+    if (length(removedSet) + length(addedSet) > 0) {
+      print("Existing:")
+      print(names(allSegments))
+      print("New:")
+      print(names(sliceSegmentationInfo))
+      print(paste(diffSet, collapse=","))
+      stop("Datasets do not match up. Please consider removing segment files.")
     }
     
-    # Add segmentation to results and write file (once in a while)
-    if ((nextSlice %% 10 == 0) | 
-          (nextSlice == nrow(sliceList)) | 
-          ((nextSlice < nrow(sliceList)) & (sliceList$Dataset[nextSlice+1] != sliceList$Dataset[nextSlice]))) {
-      ds <- sliceList$Dataset[nextSlice]
-      cat("...write", getSegmentFile(ds), "id's:",length(unique(filter(allSegments, Dataset==ds)$Id)), fill=T)
-      write.csv(filter(allSegments, Dataset==ds), getSegmentFile(ds), row.names=F)
-      
-      print("...completeness:")
-      print(group_by(left_join(sliceList, 
-                               mutate(unique(select(allSegments, Id, Slice)), isSegmented=TRUE), 
-                               by=c("Id", "Slice")), Dataset) %>% 
-              summarise(complete = paste(round(100*sum(isSegmented, na.rm=T)/n(),2),"%",sep="" )))
-      
-      # Show progress
-      ds <- group_by(left_join(sliceList, 
-                               mutate(unique(select(allSegments, Id, Slice)), isSegmented=TRUE), 
-                               by=c("Id", "Slice")), Dataset, Id) %>% 
-        summarise(complete=round(100*sum(isSegmented, na.rm=T)/n())) %>%
-        group_by(Dataset, complete) %>% summarise(n = n())
-      print(ggplot(ds, aes(x=complete, y=n, fill=Dataset))+geom_bar(stat="identity")+
-              ggtitle("Completeness of segmentation per Id"))
+    nDrop <- nrow(filter(allSegments, 
+                         Slice %in% sliceSegmentationInfo$Slice,
+                         Id %in% sliceSegmentationInfo$Id))
+    if (nDrop > 0) {
+      cat("...dropping",nDrop,"rows from previous results",fill=T)
     }
+    
+    allSegments <- rbind(filter(allSegments, 
+                                !(Slice %in% sliceSegmentationInfo$Slice & 
+                                    Id %in% sliceSegmentationInfo$Id)), 
+                         sliceSegmentationInfo)
+  }
+
+  # Add segmentation to results and write file (once in a while)
+  if ((nextSlice %% 10 == 0) | 
+        (nextSlice == nrow(sliceList)) | 
+        ((nextSlice < nrow(sliceList)) & (sliceList$Dataset[nextSlice+1] != sliceList$Dataset[nextSlice]))) {
+    ds <- sliceList$Dataset[nextSlice]
+
+    # temp add Dataset to the segments for reporting and progress info
+    allSegments <- left_join(allSegments, unique(select(imageList, Id, Slice, Time, Dataset)), by=c("Id","Slice","Time"))
+    cat("...write", getSegmentFile(ds), "id's:",length(unique(filter(allSegments, Dataset==ds)$Id)), fill=T)
+    
+    write.csv(select(filter(allSegments, Dataset==ds), -Dataset), getSegmentFile(ds), row.names=F)
+    
+    print("...completeness:")
+    print(group_by(left_join(sliceList, 
+                             mutate(unique(select(allSegments, Id, Slice)), isSegmented=TRUE), 
+                             by=c("Id", "Slice")), Dataset) %>% 
+            summarise(complete = paste(round(100*sum(isSegmented, na.rm=T)/n(),2),"%",sep="" )))
+    
+    # Show progress
+    ds <- group_by(left_join(sliceList, 
+                             mutate(unique(select(allSegments, Id, Slice)), isSegmented=TRUE), 
+                             by=c("Id", "Slice")), Dataset, Id) %>% 
+      summarise(complete=round(100*sum(isSegmented, na.rm=T)/n())) %>%
+      group_by(Dataset, complete) %>% summarise(n = n())
+    print(ggplot(ds, aes(x=complete, y=n, fill=Dataset))+geom_bar(stat="identity")+
+            ggtitle("Completeness of segmentation per Id"))
+    
+    # drop Dataset again - was only temp
+    allSegments <- select(allSegments, -Dataset)
   }
 }
 
--- fixUpSegmentsData.R
+++ fixUpSegmentsData.R
@@ -53,7 +53,8 @@ source("util.R")
 imageList <- getImageList()
 
 fixupROI <- function(imageSegments) {
-  if (nrow(imageSegments) > 1) {
+  n <- nrow(imageSegments)
+  if (n > 1) {
     rA <- imageSegments$distToROI[1:(nrow(imageSegments)-1)]
     rB <- imageSegments$distToROI[2:nrow(imageSegments)]
     xA <- imageSegments$m.cx[1:(nrow(imageSegments)-1)]
@@ -70,14 +71,20 @@ fixupROI <- function(imageSegments) {
     x2 <- round((xB+xA)/2 + (xB-xA)*f - 2*(yB-yA)*K/d2,2)
     y2 <- round((yB+yA)/2 + (yB-yA)*f + 2*(xB-xA)*K/d2,2)
     
-    # get most frequent X/Y value
-    roi.x <- round(as.double(names(sort(table(c(x1,x2)),decreasing=T))[1]))
-    roi.y <- round(as.double(names(sort(table(c(y1,y2)),decreasing=T))[1]))
-    roi.r <- round(max(imageSegments$distToROI)  )
+    if (n > 2) {
+      # get most frequent X/Y value
+      roi.x <- round(as.double(names(sort(table(c(x1,x2)),decreasing=T))[1]))
+      roi.y <- round(as.double(names(sort(table(c(y1,y2)),decreasing=T))[1]))
+      roi.r <- round(max(imageSegments$distToROI) + 5 )
+    } else {
+      roi.x <- round(mean(c(x1,x2)))
+      roi.y <- round(mean(c(y1,y2)))
+      roi.r <- round(max(imageSegments$distToROI) + 5 )
+    }
   } else {
     roi.x <- round(imageSegments$m.cx[1])
     roi.y <- round(imageSegments$m.cy[1])
-    roi.r <- round(imageSegments$distToROI[1])
+    roi.r <- round(imageSegments$distToROI[1] + 5)
   }  
   cat("ROI=",roi.x,",",roi.y," r=",roi.r,"n=",nrow(imageSegments),fill=T)
   
@@ -95,13 +102,26 @@ for (ds in unique(imageList$Dataset)) {
     dataOld <- fread(f)
     
     ### FIX UP CODE HERE
+
+    dataNew <- select(dataOld, -Dataset)
     
-    dataNew <- select(dataOld, -distToROI)
-    imgList <- unique(select(dataOld, Id, Slice, Time))
-    for (i in 678:nrow(imgList)) {
-      cat(i, "of", nrow(imgList), " %", round(100*i/nrow(imgList),2), " ")
-      fixupROI(filter(dataOld, Id == imgList$Id[i], Slice == imgList$Slice[i], Time == imgList$Time[i]))
-    }
+    #     dataNew <- select(dataOld, 
+    #                       -Dataset, -ImgType, -FileName,
+    #                       -Offset, -SliceCount, -SliceIndex, -SliceOrder, -PixelSpacing.x, -PixelSpacing.y,
+    #                       -SliceLocation, -SliceThickness,
+    #                       -isProcessed)
+    #     dataNew$m.cx <- round(dataOld$m.cx)
+    #     dataNew$m.cy <- round(dataOld$m.cy)
+    #     dataNew$m.majoraxis <- round(dataOld$m.majoraxis)
+    #     dataNew$s.area <- round(dataOld$s.area)
+    #     dataNew$s.perimeter <- round(dataOld$s.perimeter)
+    
+    #     imgList <- unique(select(dataOld, Id, Slice, Time))
+    #     for (i in 1:nrow(imgList)) {
+    #       cat(i, "of", nrow(imgList), " %", round(100*i/nrow(imgList),2), " ")
+    #       ds <- filter(dataOld, Id == imgList$Id[i], Slice == imgList$Slice[i], Time == imgList$Time[i])
+    #       fixupROI(ds)
+    #     }
     
     ###dataNew <- select(dataOld, -roundness)
     
